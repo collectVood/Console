bf513e3a99b86dddaf3bd0b0d61aa6c9a377c661
Hamster-Mute <atikinvoksert@gmail.com>
2018-10-15 14:56:08 +0500

    made a static class

--- Console/Log.cs
+++ Console/Log.cs
@@ -4,6 +4,10 @@ namespace Console
 {
     public static class Log
     {
+        /// <summary>
+        /// output a warning in the console
+        /// </summary>
+        /// <param name="warning"></param>
         public static void Warning(string warning) 
         {
             if (string.IsNullOrEmpty(warning)) return;
@@ -12,6 +16,10 @@ namespace Console
             System.Console.ResetColor();
         }
         
+        /// <summary>
+        /// output an error in the console
+        /// </summary>
+        /// <param name="error"></param>
         public static void Error(string error)
         {
             if (string.IsNullOrEmpty(error)) return;
@@ -20,6 +28,10 @@ namespace Console
             System.Console.ResetColor();
         }
         
+        /// <summary>
+        /// output a debug in the console
+        /// </summary>
+        /// <param name="debug"></param>
         public static void Debug(string debug)
         {
             if (string.IsNullOrEmpty(debug)) return;
@@ -28,6 +40,10 @@ namespace Console
             System.Console.ResetColor();
         }
         
+        /// <summary>
+        /// reads input from the console and returns a string
+        /// </summary>
+        /// <returns></returns>
         public static string Read()
         {
             var read = System.Console.ReadLine();
