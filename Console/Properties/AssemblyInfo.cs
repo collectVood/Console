dce4860d761a23606f98793176771a7aa1d0f741
Iv Misticos <21005901+IvMisticos@users.noreply.github.com>
2019-06-10 18:07:48 +0300

    Modified assembly information

--- Console/Properties/AssemblyInfo.cs
+++ Console/Properties/AssemblyInfo.cs
@@ -1,20 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Console")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Plugins, commands, hooks, etc system for console")]
 [assembly: AssemblyCompany("Iv Misticos")]
 [assembly: AssemblyProduct("Console")]
 [assembly: AssemblyCopyright("Copyright © Iv Misticos 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM components. If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
